4858f02f23de1d21354c4d2e4c95aa3748e60f92
Philip Lundrigan <philiplundrigan@gmail.com>
2015-08-18 15:04:03 -0600

    Remove wirePi
    I will require that users install it before they use this.

--- setup.py
+++ setup.py
@@ -1,18 +1,8 @@
 from distutils.core import setup, Extension
 import subprocess
 
-# Make wiringPi library
-# Make wiringPiDev library
-
-with subprocess.Popen(["make", "all"], cwd="wiringPi/wiringPi", stdout=subprocess.PIPE) as proc:
-    print(proc.stdout.read().decode('utf-8'))
-
-with subprocess.Popen(["make", "all"], cwd="wiringPi/devLib", stdout=subprocess.PIPE) as proc:
-    print(proc.stdout.read().decode('utf-8'))
-
-# setup(
-#     ext_modules=[Extension(name="sensor",
-#                            sources=["_sensor.c"],
-#                            include_dirs=["wiringPi/wiringPi", "wiringPi/devLib"],
-#                            libraries=["wiringPi", "wiringPiDev"])]
-# )
+setup(
+    ext_modules=[Extension(name="sensor",
+                           sources=["_sensor.c"],
+                           libraries=["wiringPi", "wiringPiDev"])]
+)
