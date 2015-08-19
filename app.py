a89d607c20f09013c810cfb3716bd88310396021
Philip Lundrigan <philiplundrigan@gmail.com>
2015-08-19 00:08:20 -0400

    Add data to firebase

--- app.py
+++ app.py
@@ -1,7 +1,30 @@
 import sensor
+from flask import Flask
+# from flask_restful import Resource, Api
+# from pymongo import MongoClient
+from datetime import datetime
+from firebase import firebase
 
-def writer(temperature, humdity):
-    print(temperature)
-    print(humdity)
+# client = MongoClient()
+# db = client.measurements_db
+# measurements = db.measurements_collection
 
-sensor.read(writer)
+firebase = firebase.FirebaseApplication('https://temperature-sensor.firebaseio.com', None)
+
+app = Flask(__name__)
+# api = Api(app)
+
+def writer(temperature, humidity):
+    measurement = {"temperature": temperature,
+                   "humidity": humidity,
+                   "date": datetime.utcnow()}
+
+    result = firebase.post('/measurements', measurement)
+    print(result)
+
+    # measurements.insert_one(measurement)
+
+
+if __name__ == '__main__':
+    sensor.read(writer)
+    app.run(debug=True)
