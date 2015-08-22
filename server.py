b394cb852bab75842d6f187b187b86f7f277abc8
Philip Lundrigan <philiplundrigan@gmail.com>
2015-08-21 20:06:37 -0400

    Add method to cache requested data

--- server.py
+++ server.py
@@ -3,53 +3,52 @@ from datetime import datetime
 from firebase import firebase
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 import dateutil.parser
+import time
 
 def parse_time(time):
     if type(time) is str:
         return dateutil.parser.parse(time)
     else:
-        return time
+        return datetime.fromtimestamp(time / 1000)
 
 def get_data():
     fb = firebase.FirebaseApplication('https://temperature-sensor.firebaseio.com', None)
-    results = fb.get('/measurements', None)
+    last_get_data = int(time.time() * 1000)
+
+    print("getting new data")
+    results = fb.get('', 'measurements', params={'orderBy': '"date"'})
     results = list(results.values())
 
     # Set up data frame
     df = pd.DataFrame(results)
     df['date'] = df['date'].map(parse_time)
     df = df.set_index('date')
+    df = df.sort()
 
     # Convert C to F
     df['temperature'] = df['temperature'] * 9/5 + 32
 
-    return df
+    def _get_more_data():
+        nonlocal df
+        nonlocal last_get_data
+
+        results = fb.get('', 'measurements', params={'orderBy': '"date"', 'startAt': last_get_data})
+        results = list(results.values())
+
+        print(results)
+
+        # Add to data frame
+
+        last_get_data = int(time.time() * 1000)
+        return df
 
 
-df = get_data()
+    return df, _get_more_data
 
-print(df.describe())
 
-# fig = plt.figure()
-# ax = fig.add_subplot(111)
 
-# ax.plot(df['temperature'])
-# ax.plot(pd.ewma(df['temperature'], span=60))
-# ax.plot(pd.rolling_mean(df['temperature'], window=60))
 
-# ax.plot(time, temperature, '.')
-# ax.plot(time, humidity, '.')
-
-# ax.set_ylim(0)
-
-# plt.show()
-
-exit()
-
-app = Flask(__name__)
 
 if __name__ == '__main__':
     app.run(debug=True)
