41a31312af8b7c6c656d20578843e7b7d3531f65
Philip Lundrigan <philiplundrigan@gmail.com>
2015-08-21 23:31:09 -0400

    Append new data to old data

--- server.py
+++ server.py
@@ -6,20 +6,15 @@ import pandas as pd
 import dateutil.parser
 import time
 
+
 def parse_time(time):
     if type(time) is str:
         return dateutil.parser.parse(time)
     else:
         return datetime.fromtimestamp(time / 1000)
 
-def get_data():
-    fb = firebase.FirebaseApplication('https://temperature-sensor.firebaseio.com', None)
-    last_get_data = int(time.time() * 1000)
 
-    print("getting new data")
-    results = fb.get('', 'measurements', params={'orderBy': '"date"'})
-    results = list(results.values())
-
+def format_data(results):
     # Set up data frame
     df = pd.DataFrame(results)
     df['date'] = df['date'].map(parse_time)
@@ -29,16 +24,25 @@ def get_data():
     # Convert C to F
     df['temperature'] = df['temperature'] * 9/5 + 32
 
+    return df
+
+
+def get_data():
+    fb = firebase.FirebaseApplication('https://temperature-sensor.firebaseio.com', None)
+    last_get_data = int(time.time() * 1000)
+
+    results = fb.get('', 'measurements', params={'orderBy': '"date"'})
+    results = list(results.values())
+    df = format_data(results)
+
     def _get_more_data():
         nonlocal df
         nonlocal last_get_data
 
         results = fb.get('', 'measurements', params={'orderBy': '"date"', 'startAt': last_get_data})
         results = list(results.values())
-
-        print(results)
-
-        # Add to data frame
+        new_df = format_data(results)
+        df = df.append(new_df)
 
         last_get_data = int(time.time() * 1000)
         return df
