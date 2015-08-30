e2eca80a0c3eaef3938ebd3d811cf331e7e27cae
Philip Lundrigan <philiplundrigan@gmail.com>
2015-08-30 15:00:04 -0600

    Fix precision of temp, rh, and date

--- collect.py
+++ collect.py
@@ -7,9 +7,9 @@ if __name__ == '__main__':
     firebase = firebase.FirebaseApplication('https://temperature-sensor.firebaseio.com', None)
 
     def writer(temperature, humidity):
-        measurement = {"temperature": temperature,
-                       "humidity": humidity,
-                       "date": time.time() * 1000}
+        measurement = {"temperature": round(temperature, 2),
+                       "humidity": round(humidity, 2),
+                       "date": int(time.time() * 1000)}
 
         result = firebase.post('/measurements', measurement)
         print(result)
