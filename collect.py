b57eaec78d5aad538e6a93ab3bff466656eda86d
Philip Lundrigan <philiplundrigan@gmail.com>
2015-08-30 15:02:46 -0600

    Try to catch errors if firebase database has disconnected

--- collect.py
+++ collect.py
@@ -3,15 +3,22 @@ import time
 from datetime import datetime
 from firebase import firebase
 
+firebase_name = 'https://temperature-sensor.firebaseio.com'
+
 if __name__ == '__main__':
-    firebase = firebase.FirebaseApplication('https://temperature-sensor.firebaseio.com', None)
+    firebase = firebase.FirebaseApplication(firebase_name, None)
 
     def writer(temperature, humidity):
-        measurement = {"temperature": round(temperature, 2),
-                       "humidity": round(humidity, 2),
-                       "date": int(time.time() * 1000)}
+        try:
+            measurement = {"temperature": round(temperature, 2),
+                           "humidity": round(humidity, 2),
+                           "date": int(time.time() * 1000)}
 
-        result = firebase.post('/measurements', measurement)
-        print(result)
+            result = firebase.post('/measurements', measurement)
+            print(result)
+        except Exception as e:
+            print(e)
+            print("Trying to reconnect...")
+            firebase = firebase.FirebaseApplication(firebase_name, None)
 
     sensor.read(writer)
