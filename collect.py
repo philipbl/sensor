fcc945612a339dc8af4c4dfbbe6be0b39f4a1369
Philip Lundrigan <philiplundrigan@gmail.com>
2015-08-30 15:22:06 -0600

    Fix syntax errors

--- collect.py
+++ collect.py
@@ -5,20 +5,25 @@ from firebase import firebase
 
 firebase_name = 'https://temperature-sensor.firebaseio.com'
 
-if __name__ == '__main__':
-    firebase = firebase.FirebaseApplication(firebase_name, None)
+def start_writer():
+    fb = firebase.FirebaseApplication(firebase_name, None)
 
     def writer(temperature, humidity):
+        nonlocal fb
+
         try:
             measurement = {"temperature": round(temperature, 2),
                            "humidity": round(humidity, 2),
                            "date": int(time.time() * 1000)}
 
-            result = firebase.post('/measurements', measurement)
+            result = fb.post('/measurements', measurement)
             print(result)
         except Exception as e:
             print(e)
             print("Trying to reconnect...")
-            firebase = firebase.FirebaseApplication(firebase_name, None)
+            fb = firebase.FirebaseApplication(firebase_name, None)
 
-    sensor.read(writer)
+    return writer
+
+if __name__ == '__main__':
+    sensor.read(start_writer())
