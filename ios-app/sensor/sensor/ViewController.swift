347b353e4805562d3e9cda2988f692e1f4435ccc
Philip Lundrigan <philiplundrigan@gmail.com>
2015-09-02 14:07:24 -0600

    Start Xcode project

---
+++ ios-app/sensor/sensor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sensor
+//
+//  Created by Philip Lundrigan on 9/2/15.
+//  Copyright (c) 2015 Philip Lundrigan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
