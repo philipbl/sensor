36e3c6f0f8ec1cb692acf58d3e596054f3a28049
Philip Lundrigan <philiplundrigan@gmail.com>
2015-09-16 20:47:09 -0600

    Update tests with new alerts object

--- server/test_alerts.py
+++ server/test_alerts.py
@@ -16,34 +16,32 @@ def test_init(fb):
     # Delete everything
     fb.delete(dummy_database, None)
 
-    alerts = Alerts(dummy_database)
+    alerts = Alerts(lambda x: x, dummy_database)
     assert len(alerts.alerts) == 0
 
 
 def test_previous_data(fb):
-    email = "test@test"
+    id = "test@test"
     type = "temperature"
     bound = 80
     direction = "gt"
 
     # Add previous data
     fb.post(dummy_database,
-            {"email": email, "type": type, "bound": bound, "direction": direction})
+            {"id": id, "type": type, "bound": bound, "direction": direction})
 
-    alerts = Alerts(dummy_database)
-
-    print(alerts.alerts)
+    alerts = Alerts(lambda x: x, dummy_database)
 
     assert alerts.alerts is not None
     assert len(alerts.alerts) == 1
-    assert alerts.alerts.ix[0].email == email
+    assert alerts.alerts.ix[0].id == id
     assert alerts.alerts.ix[0].type == type
     assert alerts.alerts.ix[0].bound == bound
     assert alerts.alerts.ix[0].direction == operator.gt if direction == 'gt' else operator.lt
 
 
 def test_add(fb):
-    email = "test@test"
+    id = "test@test"
     type = "temperature"
     bound = 80
     direction = "gt"
@@ -52,31 +50,31 @@ def test_add(fb):
     fb.delete(dummy_database, None)
 
     # Create new alerts
-    alerts = Alerts(dummy_database)
+    alerts = Alerts(lambda x: x, dummy_database)
     assert len(alerts.alerts) == 0
 
     # Add one alert
-    alerts.add_alert(email, type, bound, direction)
+    alerts.add_alert(id, type, bound, direction)
 
     assert len(alerts.alerts) == 1
-    assert alerts.alerts.ix[0].email == email
+    assert alerts.alerts.ix[0].id == id
     assert alerts.alerts.ix[0].type == type
     assert alerts.alerts.ix[0].bound == bound
     assert alerts.alerts.ix[0].direction == operator.gt if direction == 'gt' else operator.lt
 
     # Try adding again
-    alerts.add_alert(email, type, bound, direction)
+    alerts.add_alert(id, type, bound, direction)
 
     # Nothing should have changed
     assert len(alerts.alerts) == 1
-    assert alerts.alerts.ix[0].email == email
+    assert alerts.alerts.ix[0].id == id
     assert alerts.alerts.ix[0].type == type
     assert alerts.alerts.ix[0].bound == bound
     assert alerts.alerts.ix[0].direction == operator.gt if direction == 'gt' else operator.lt
 
 
 def test_delete(fb):
-    email = "test@test"
+    id = "test@test"
     type = "temperature"
     bound = 80
     direction = "gt"
@@ -86,21 +84,21 @@ def test_delete(fb):
 
     # Add previous data
     fb.post(dummy_database,
-            {"email": email, "type": type, "bound": bound, "direction": direction})
+            {"id": id, "type": type, "bound": bound, "direction": direction})
 
-    alerts = Alerts(dummy_database)
+    alerts = Alerts(lambda x: x, dummy_database)
 
     # Delete alert
-    alerts.delete_alert(email, type, bound, direction)
+    alerts.delete_alert(id, type, bound, direction)
     assert len(alerts.alerts) == 0
 
     # Try deleting alert again
-    alerts.delete_alert(email, type, bound, direction)
+    alerts.delete_alert(id, type, bound, direction)
     assert len(alerts.alerts) == 0
 
 
 def test_add_and_delete(fb):
-    email = "test@test"
+    id = "test@test"
     type = "temperature"
     bound = 80
     direction = "gt"
@@ -108,22 +106,22 @@ def test_add_and_delete(fb):
     # Delete everything
     fb.delete(dummy_database, None)
 
-    alerts = Alerts(dummy_database)
+    alerts = Alerts(lambda x: x, dummy_database)
     assert len(alerts.alerts) == 0
 
-    alerts.add_alert(email, type, bound, direction)
+    alerts.add_alert(id, type, bound, direction)
     assert len(alerts.alerts) == 1
-    assert alerts.alerts.ix[0].email == email
+    assert alerts.alerts.ix[0].id == id
     assert alerts.alerts.ix[0].type == type
     assert alerts.alerts.ix[0].bound == bound
     assert alerts.alerts.ix[0].direction == operator.gt if direction == 'gt' else operator.lt
 
-    alerts.delete_alert(email, type, bound, direction)
+    alerts.delete_alert(id, type, bound, direction)
     assert len(alerts.alerts) == 0
 
 
 def test_clear(fb):
-    email = "test@test"
+    id = "test@test"
     type = "temperature"
     bound = 80
     direction = "gt"
@@ -131,12 +129,12 @@ def test_clear(fb):
     # Delete everything
     fb.delete(dummy_database, None)
 
-    alerts = Alerts(dummy_database)
+    alerts = Alerts(lambda x: x, dummy_database)
     assert len(alerts.alerts) == 0
 
-    alerts.add_alert(email, type, bound, direction)
+    alerts.add_alert(id, type, bound, direction)
     assert len(alerts.alerts) == 1
-    assert alerts.alerts.ix[0].email == email
+    assert alerts.alerts.ix[0].id == id
     assert alerts.alerts.ix[0].type == type
     assert alerts.alerts.ix[0].bound == bound
     assert alerts.alerts.ix[0].direction == operator.gt if direction == 'gt' else operator.lt
