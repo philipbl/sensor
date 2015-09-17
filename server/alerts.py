f5e34996968437d6421a4d762d43458e71f9df8d
Philip Lundrigan <philiplundrigan@gmail.com>
2015-09-16 20:17:49 -0600

    Change email to ID
    This way it can be general with any type of alert

--- server/alerts.py
+++ server/alerts.py
@@ -30,7 +30,7 @@ class Alerts(object):
 
         if results is None:
             logging.info("There are no stored alerts")
-            return pd.DataFrame([], columns=['type', 'bound', 'direction', 'email', 'active'])
+            return pd.DataFrame([], columns=['type', 'bound', 'direction', 'id', 'active'])
 
         data = []
         for key, value in results.items():
@@ -47,7 +47,7 @@ class Alerts(object):
 
     def _trigger_alert(self, trigger, current):
         logging.info("Triggering an alert!")
-        print("Send email to {} saying:".format(trigger.email))
+        print("Send email to {} saying:".format(trigger.id))
         print("{type} has gone {direction} {bound} ({current}).".format(type=trigger.type,
                                                                         direction="above" if trigger.direction == operator.gt else "below",
                                                                         bound=trigger.bound,
@@ -59,8 +59,8 @@ class Alerts(object):
         data = self._get_latest_data()
         df = self.alerts
 
-        for i in range(len(df)):
-            series = df.ix[i]
+        for i in range(len(self.alerts)):
+            series = self.alerts.ix[i]
 
             if series.direction(data[series.type], series.bound):
                 logging.info("Condition has been met!")
@@ -74,7 +74,7 @@ class Alerts(object):
                 logging.info("Turning off active")
                 df.loc[i, 'active'] = False
 
-    def _find_alert(self, email, type, bound, direction):
+    def _find_alert(self, id, type, bound, direction):
         if self.alerts is None:
             return []
 
@@ -84,7 +84,7 @@ class Alerts(object):
         alert = df[(df.type == type) &
                    (df.bound == bound) &
                    (df.direction == direction) &
-                   (df.email == email)]
+                   (df.id == id)]
 
         return alert
 
@@ -92,33 +92,33 @@ class Alerts(object):
         alerts = self._trigger_alerts()
         threading.Timer(60, self.run).start()
 
-    def add_alert(self, email, type, bound, direction):
-        logging.info("Adding new alert: %s %s %s %s", email, type, bound, direction)
+    def add_alert(self, id, type, bound, direction):
+        logging.info("Adding new alert: %s %s %s %s", id, type, bound, direction)
 
         bound = float(bound)
-        alert = self._find_alert(email, type, bound, direction)
+        alert = self._find_alert(id, type, bound, direction)
         if len(alert) > 0:
             logging.info("Alert is already contained in database")
             return
 
         # Store alert in database
         result = self.fb.post(self.database,
-                             {"email": email, "type": type, "bound": bound, "direction": direction})
+                             {"id": id, "type": type, "bound": bound, "direction": direction})
 
         # Keep track of alert locally
         direction = operator.gt if direction == 'gt' else operator.lt
-        s = pd.Series([type, bound, direction, email, False],
-                      index=['type', 'bound', 'direction', 'email', 'active'],
+        s = pd.Series([type, bound, direction, id, False],
+                      index=['type', 'bound', 'direction', 'id', 'active'],
                       name=result['name'])
         self.alerts = self.alerts.append(s)
 
         logging.info("New alerts:\n %s", self.alerts)
 
-    def delete_alert(self, email, type, bound, direction):
-        logging.info("Deleting alert: %s %s %s %s", email, type, bound, direction)
+    def delete_alert(self, id, type, bound, direction):
+        logging.info("Deleting alert: %s %s %s %s", id, type, bound, direction)
 
         bound = float(bound)
-        alert = self._find_alert(email, type, bound, direction)
+        alert = self._find_alert(id, type, bound, direction)
         if len(alert) == 0:
             logging.info("There is no alert to delete")
             return
@@ -136,4 +136,4 @@ class Alerts(object):
         self.fb.delete(self.database, None)
 
         # Clear locally
-        self.alerts = pd.DataFrame([], columns=['type', 'bound', 'direction', 'email', 'active'])
+        self.alerts = pd.DataFrame([], columns=['type', 'bound', 'direction', 'id', 'active'])
